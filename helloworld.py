8f684f16b34a7eca9587ac93d03a477ef2609c8b
dominikwdr <91007130+dominikwdr@users.noreply.github.com>
2021-11-05 17:54:38 +0100

    Behebung Problem

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,22 @@
-print("Hello World! :D")
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World!")
+
+
+@app.route("/hello")
+def hello():
+    return render_template('index.html', name="Dominik")
+
+
+@app.route("/test")
+def test():
+    return "done!"
+
+@app.route("/done")
+def done():
+    return "abgeschickt!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
