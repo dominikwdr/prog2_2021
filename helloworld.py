ee76f561d48e9ba53a65d3b0f6302b326e1bd7c2
dominikwdr <91007130+dominikwdr@users.noreply.github.com>
2022-01-03 19:54:54 +0100

    Stand 03.01.2022

--- helloworld.py
+++ helloworld.py
@@ -4,9 +4,13 @@ from flask import render_template
 app = Flask("Hello World!")
 
 
-@app.route("/hello")
-def hello():
+@app.route("/index")
+def index():
     return render_template('index.html', name="Dominik")
+
+@app.route("/input")
+def input():
+    return render_template('input.html', name="Dominik")
 
 
 @app.route("/test")
