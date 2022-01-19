69d4c9e8a45674c52213ee08816d5be38571978c
dominikwdr <91007130+dominikwdr@users.noreply.github.com>
2022-01-19 13:04:52 +0100

    Final_19.01.2022

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from flask import render_template
 app = Flask("XPENSE")
 
 
-#Index-Seite
+#Index / Page "Dashboard"
 
 @app.route("/")
 def index():
-    e = open("data/expenses.json")
+    e = open("data/expenses.json") #Datenabfrage von Ausgabe-Daten
     expenses_data = json.load(e)
-    i = open("data/income.json")
+    i = open("data/income.json") #Datenabfrage von Einnahme-Daten
     income_data = json.load(i)
 
+    #Unterteilung der Daten in Kategorien oder Transaktionen von Einnahmen/Ausgaben
     expenses_categories = expenses_data["categories"]
     expenses_transactions = expenses_data["transactions"]
 
@@ -24,30 +25,36 @@ def index():
     income_transactions = income_data["transactions"]
 
 
-    #Berechnungen
+    #Berechnungen für die Statistiken
 
-    number_expenses = len(expenses_data)
-    total_expenses = float(0.00)
+    #Definierung von einzelnen Werten
+    number_expenses = len(expenses_data) #Formatierungen in "length", um Durchschnitt an Ausgaben/Einnahme zu berechnen
+    total_expenses = float(0.00) #Formatierungen in "float", da Geldbeträge auf 5 Rappen genau (also 2 Dezimalstellen)
     average_expense = float(0.00)
     number_income = len(income_data)
     total_income = float(0.00)
     highest_expense = float(0.00)
     highest_income = float(0.00)
 
+    #Berechnungen Ausgaben
     if number_expenses > 0:
-
         for entry in expenses_transactions:
-            total_expenses = total_expenses + float(str(expenses_transactions[entry]["amount"]))
-            if float(str(expenses_transactions[entry]["amount"])) > highest_expense:
-                highest_expense = float(str(expenses_transactions[entry]["amount"]))
+            total_expenses = total_expenses + float(expenses_transactions[entry]["amount"])
+            if float(expenses_transactions[entry]["amount"]) > highest_expense:
+                highest_expense = float(expenses_transactions[entry]["amount"])
         average_expense = round(total_expenses / number_expenses, 2)
 
+
+    #Berechnugnen Einnahmen
     if number_income > 0:
         for entry in income_transactions:
             total_income += float(income_transactions[entry]["amount"])
-            if float(str(income_transactions[entry]["amount"])) > highest_expense:
-                highest_income = float(str(income_transactions[entry]["amount"]))
+            if float(income_transactions[entry]["amount"]) > highest_income:
+                highest_income = float(income_transactions[entry]["amount"])
 
+
+
+    #Ausgabe der Statistik-Werte für Übersicht
     statistics = {
         "Einnahmen total": total_income,
         "Ausgaben total": total_expenses,
@@ -57,57 +64,59 @@ def index():
         "Durchschnittliche Ausgabe pro Transaktion": average_expense
     }
 
-
-    with open('data/expenses.json', 'w') as f:
-        json.dump(expenses_data, f, indent=4, separators=(',', ':'))
-    with open('data/income.json', 'w') as f:
-        json.dump(income_data, f, indent=4, separators=(',', ':'))
-
     return render_template('index.html', expenses_transactions=expenses_transactions,
                            expenses_categories=expenses_categories, income_transactions=income_transactions,
                            income_categories=income_categories, statistics=statistics)
 
 
 
-
-#Input für Geldausgaben
+#Input für Geldausgaben / Page "Ausgabe erfassen"
 
 @app.route("/input-expenses", methods=['GET', 'POST'])
 def input_expenses():
-    e = open("data/expenses.json")
+    e = open("data/expenses.json") #Datenabfrage der Ausgaben
     expenses_data = json.load(e)
 
+    #Unterteilung der Daten in Kategorien oder Transaktionen von Ausgaben
     expenses_categories = expenses_data["categories"]
     expenses_transactions = expenses_data["transactions"]
 
     if request.method == 'POST':
-        if "category_name" in request.form:
-            category_name = request.form["category_name"]
-            category_color = request.form["category_color"]
+        if "category_name" in request.form: #Abfrage, ob neuer Eintrag auf Page "Ausgabe erfassen" eine Kategorie ist
+            if request.form["category_name"] != "": #Abfrage ob request.form Werte enthält
+                category_name = request.form["category_name"] #Auswerten der Eingaben aus dem Formular (Name der Kategorie)
+                category_color = request.form["category_color"] #Auswerten der Eingaben aus dem Formular (Farbe im HEX-Code)
 
-            expenses_categories[category_name] = {
-                "name": category_name,
-                "color": category_color
-            }
+                #Ausgabe der Ausgabenkategorien
+                expenses_categories[category_name] = {
+                    "name": category_name, #"categorie_name" doppelt, für einfachere Ausgabe und doch einheitliche Gliederung
+                    "color": category_color #HEX-Code der Farbe, welche für Kategorie definiert werden muss
+                }
 
-        if "transaction_title" in request.form:
-            datetime_now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
-            dt_obj = datetime.datetime.strptime(str(datetime_now), '%d.%m.%Y %H:%M:%S')
-            datetime_now_ms = int(round(dt_obj.timestamp() * 1000))
+        if "transaction_title" in request.form: #Abfrage, ob neuer Eintrag eine Transaktion ist
+            if request.form["transaction_title"] != "": #Abfrage ob request.form Werte enthält
+                #Erstellung einer individuellen ID pro Transaktion mittels Timestamp auf Millisekunde genau
+                datetime_now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+                dt_obj = datetime.datetime.strptime(str(datetime_now), '%d.%m.%Y %H:%M:%S')
+                datetime_now_ms = int(round(dt_obj.timestamp() * 1000))
+                #Quelle/Tipp zu dieser Methode: Felix (Freund)
 
-            transaction_id = datetime_now_ms
-            transaction_datetime = datetime_now
-            transaction_title = request.form["transaction_title"]
-            transaction_amount = request.form["transaction_amount"]
-            transaction_category = request.form["transaction_category"]
+                #Übersicht der einzelnen Daten aus request.form
+                transaction_id = datetime_now_ms
+                transaction_datetime = datetime_now
+                transaction_title = request.form["transaction_title"]
+                transaction_amount = request.form["transaction_amount"]
+                transaction_category = request.form["transaction_category"]
 
-            expenses_transactions[transaction_id] = {
-                "title": transaction_title,
-                "amount": transaction_amount,
-                "category": transaction_category,
-                "datetime": transaction_datetime
-            }
+                # Daten gegliedert für Dump in expenses.json
+                expenses_transactions[transaction_id] = {
+                    "title": transaction_title,
+                    "amount": transaction_amount,
+                    "category": transaction_category,
+                    "datetime": transaction_datetime
+                }
 
+    #Dump Daten in expenses.json
     with open('data/expenses.json', 'w') as f:
         json.dump(expenses_data, f, indent=4, separators=(',', ':'))
 
@@ -115,54 +124,60 @@ def input_expenses():
                            expenses_categories=expenses_categories)
 
 
-
-#Input für Geldeinnahmen
+#Input für Geldeinnahmen / Page "Einnahme erfassen"
 
 @app.route("/input-income", methods=['GET', 'POST'])
 def input_income():
-    i = open("data/income.json")
+    i = open("data/income.json") #Datenabfrage der Einnahmen
     income_data = json.load(i)
 
+    #Unterteilung der Daten in Kategorien oder Transaktionen von Einnahmen
     income_categories = income_data["categories"]
     income_transactions = income_data["transactions"]
 
     if request.method == 'POST':
-        if "category_name" in request.form:
-            category_name = request.form["category_name"]
+        if "category_name" in request.form: #Abfrage, ob neuer Eintrag eine Kategorie ist
+            if request.form["category_name"] != "": #Abfrage ob request.form Werte enthält
+                category_name = request.form["category_name"] #Auswerten der Eingaben aus dem Formular (Name der Kategorie)
+                category_color = request.form["category_color"] #Auswerten der Eingaben aus dem Formular (Farbe im HEX-Format)
 
-            income_categories[category_name] = {
-                "name": category_name
-            }
-
-
-        if "transaction_title" in request.form:
-            datetime_now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
-            dt_obj = datetime.datetime.strptime(str(datetime_now), '%d.%m.%Y %H:%M:%S')
-            datetime_now_ms = int(round(dt_obj.timestamp() * 1000))
-
-            transaction_id = datetime_now_ms
-            transaction_datetime = datetime_now
-            transaction_title = request.form["transaction_title"]
-            transaction_amount = request.form["transaction_amount"]
-            transaction_category = request.form["transaction_category"]
-
-            income_transactions[transaction_id] = {
-                "title": transaction_title,
-                "amount": transaction_amount,
-                "category": transaction_category,
-                "datetime": transaction_datetime
-            }
+                #Ausgabe der Einnahmekategorien
+                income_categories[category_name] = {
+                    "name": category_name, #"categorie_name" doppelt, für einfachere Ausgabe und doch einheitliche Gliederung
+                    "color": category_color #HEX-Code der Farbe, welche für Kategorie definiert werden muss
+                }
 
 
+        if "transaction_title" in request.form: #Abfrage, ob neuer Eintrag eine Transaktion ist
+            if request.form["transaction_title"] != "": #Abfrage ob request.form Werte enthält
+                # Erstellung einer individuellen ID pro Transaktion mittels Timestamp auf Millisekunde genau
+                datetime_now = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+                dt_obj = datetime.datetime.strptime(str(datetime_now), '%d.%m.%Y %H:%M:%S')
+                datetime_now_ms = int(round(dt_obj.timestamp() * 1000))
+                #Quelle/Tipp zu dieser Methode: Felix (Freund)
+
+                #Übersicht der einzelnen Daten aus request.form
+                transaction_id = datetime_now_ms
+                transaction_datetime = datetime_now
+                transaction_title = request.form["transaction_title"]
+                transaction_amount = request.form["transaction_amount"]
+                transaction_category = request.form["transaction_category"]
+
+                #Daten gegliedert für Dump in income.json
+                income_transactions[transaction_id] = {
+                    "title": transaction_title,
+                    "amount": transaction_amount,
+                    "category": transaction_category,
+                    "datetime": transaction_datetime
+                }
+
+        # Dump Daten in income.json
         with open('data/income.json', 'w') as f:
             json.dump(income_data, f, indent=4, separators=(',', ':'))
 
     return render_template('input_income.html', income_transactions=income_transactions,
                            income_categories=income_categories)
 
-
-
-#Rest Python
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- readme.md
+++ readme.md
@@ -1,15 +1,49 @@
-# prog2_2021
+# XPENSE - PRO2_DBM20VZ - Dominik Werder
 
-Bis 06.01.2022
-- Verschiedene Anpassungen im Grundcode
-- "Grundgerüst" aufgebaut
-- Bootstrap eingebunden
-- Verschiedene Felder eingefügt zur zukünftigen Eingabe
-- "input_expenses.html" erstellt - für Eingabe von Expenses und Kategorien
+Todo:
+- Erklärungen von Überlegungen und Struktur des Codes in Form von Kommentaren und Funktionsbeschreibungen im Code.
+- Eine Projektbeschreibung mit Überlegungen und Entstehungsdetails mit Projektidee und Diagramme.
+- Anleitung oder Walkthrough wie das Projekt installiert und benutzt wird, welche Funktionen vorhanden sind und wie diese eingesetzt werden.
 
-Stand 06.01.2022
-- Code aufgeräumt. Neue Files erstellt, worin Teile des "index.html" ausgelagert werden können
-  - navigation.html
-  - html_head.html
-- Somit habe ich bessere Übersicht erhalten
 
+## Projektbeschreibung
+### Über XPENSE
+XPENSE ist ein Programm zur Übersicht der eigenen Einnahmen und Ausgaben, also ein kleines Buchhaltungsprogramm.
+Durch das Internet und Kartenzahlungen gibt jede Person sein Geld oft unbewusst aus. XPENSE soll hier Abhilfe schaffen und vor allem das Ausgeben von Geld bewusster gestalten. Es können einzelne Einnahmen und Ausgaben (mit einigen Details) erfasst werden, welche dann in einer Übersicht dargestellt werden. Somit soll das eigene Vermögen immer im Überblick gehalten werden können.
+ 
+
+### Überlegungen und Entstehungsdetails
+Das Programm XPENSE soll helfen, Ausgaben bewusster zu tätigen. Zudem sollen die Einnahmen erfasst werden können, um gut Statistik führen zu können. Die ersten Überlegungen waren die Anforderungen an das System:
+* Ausgaben/Einnahmen erfassen
+* Ausgabe/Einnahmen nach Kategorie einordenen
+* Neue Kategorien erfassen
+* Dashboardübersicht
+* Verschiedene Kennzahlen
+
+Anhand dieser Anforderungen wurde ein grober Systemaufbau erstellt. Der angepasste und digitalisierte Aufbau befindet sich im nächsten Abschnitt. Im Enstehungsprozess kamen weitere Funktionen hinzu. Die Kategorien können nun mit einer Farbe versehen werden, was zu einer besseren Übersichtlichkeit führt. Zudem werden den Transaktionen automatisch die aktuelle Zeit der Eintragung zugeordnet, was die Eingabe erleichtert. Zudem war nur eine Eingabe-Seite geplant. Ebenfalls wegen der besseren Übersicht und einfacheren Handhabung wurden daraus zwei Seite ("Einnahme erfassen" und "Ausgabe erfassen"). 
+
+### Übersichtsdiagramm XPENSE
+![](images/XPENSE_Diagram.jpg)
+
+## Anleitung XPENSE
+Die Bedienung von XPENSE ist übersichtlich. Zur Hilfe kann das Diagramm aus vorherigem Absatz beigezogen werden.
+Das Programm besteht aus folgenden Seiten:
+
+### Dashboard
+Im Dashboard befindet sich eine Übersicht über die getätigten Transaktionen. Zudem eine Statistik, mit verschiedenen, relevanten Kennzahlen. Es können nur Werte und Informationen abgelesen werden, aber keine Eingaben getätigt werden.
+
+### Ausgaben/Einnahmen erfassen
+Folgende Bedienungsmöglichkeiten bieten die Seiten "Ausgaben erfassen" und "Einnahmen erfassen":
+* Neue Ausgabe/Einnahme erfassen
+  * Ein Titel und der Betrag der Ausgabe/Einnahme soll eingegeben werden
+  * Zudem soll eine Kategorie ausgewählt werden (ACHTUNG: Zuerst muss eine Kategorie erstellt werden)
+  * Nach betätigen des "erfassen"-Button wird die Transaktion eingetragen und im Dashboard angezeigt
+
+* Kategorie erstellen
+  * Es muss ein Namen für eine Kategorie, welche neu erstellt werden soll, erfasst werden
+  * Zudem muss über den "Color Picker", eine Farbe, welche die Kategorie haben soll, ausgewählt werden
+  * Nach betätigen des "erstellen"-Button wird die Kategorie eingetragen und kann nun genutzt werden
+  * ACHTUNG: Kategorien sind für Einnahmen und Ausgaben nicht identisch
+
+* Bestehende Kategorien
+  * Hier befinden sich die bereits erstellten Kategorien, welche als Hilfe (beim erstellen einer neue Kategorie), abgebildet werden
